6a9d1efc6315b437e2541afb9d47c6ca356f62c3
shimonheimowitz <heimy4prez@gmail.com>
2018-01-02 14:36:49 +0200

    working on symbol table

--- JackCompiler/SymbolTable.py
+++ JackCompiler/SymbolTable.py
@@ -23,27 +23,47 @@ class SymbolTable:
     """
 
     """
-    def __init__(self, table_scope):
+    def __init__(self, parent_scope=None):
         """
-
-        :param table_typle:
+        Generate a new level of the table, by creating a new scope. The parent's scope
+        must be sent in the constructor. Only the highest scope should be none.
+        :param  parent_scope:
         """
-        self.scope = table_scope
+        self.parent_scope = parent_scope
         self.table = {}
         self.counters = {
+            "local"     : 0,
             "argument"  : 0,
             "var"       : 0,
             "static"    : 0,
             "field"     : 0
         }
+        self.retrieved = None
 
     def in_table(self, name):
         """
-        Searches for a specific named symbol in the table
+        Searches for a specific named symbol in the table, working its way up the scopes
         :param name:
         :return:
         """
-        return name in self.table.keys()
+        self.retrieved = None
+        found = name in self.table.keys()
+        if found:
+            self.retrieved = self.table[name]
+        elif self.parent_scope:
+            found = self.parent_scope.in_table(name)
+            self.retrieved = self.parent_scope._retrieve()
+        return found
+
+    def _retrieve(self):
+        """
+        Should only be called after in_table, and returned true. Returns the the relevant
+        information of a
+         given symbol name
+        :return:
+        """
+        assert self.retrieved
+        return self.retrieved
 
     def get_number(self, name):
         """
@@ -52,13 +72,47 @@ class SymbolTable:
         :param name:
         :return:
         """
-        try:
-           num = self.table[name][NUM]
-           return num
-        except KeyError:
+        if self.in_table(name):
+            return self.retrieved[NUM]
+        else:
             return -1
 
+    def get_kind(self, name):
+        """
+        Returns the kind of the specific name in the table. If the
+        name is not in the table or it's parent scopes, returns None
+        :param name:
+        :return:
+        """
+        if self.in_table(name):
+            return self.retrieved[KIND]
+        else:
+            return None
+
+    def get_type(self, name):
+        """
+        Returns the number of the specific name in the table. If the
+        name is not in the table or it's parent scopes, returns None
+        :param name:
+        :return:
+        """
+        if self.in_table(name):
+            return self.retrieved[TYPE]
+        else:
+            return None
+
+
     def add_symbol(self, name, type, kind):
+        """
+        Adds a new symbol into the table. Symbol assumed not to exist in current scope
+        :param name:
+        :param type:
+        :param kind:
+        :return:
+        """
+
+        # We may want to add the statics into the heighest scope or something
+
         assert name not in self.table.keys()
         if kind in self.counters.keys():
             num = self.counters[kind]
@@ -68,12 +122,24 @@ class SymbolTable:
         # np.append(self.table, [name, type, kind, num])
         self.table[name] = [type, kind, num]
 
-    def clean_non_static(self):
+    def _clean_non_static(self):
         """
         Erases all entries in this table except the static values
         :return:
         """
         self.table = {k: v for k, v in self.table.items() if "static" == v[TYPE]}
+
+    def kill_scope(self):
+        """
+        kills current scope, by saving all the statics and destorying the current layer.
+        :return:
+        """
+        self._clean_non_static()
+        self.parent_scope._add_statics(self.table)
+
+    def _add_statics(self, statics):
+        self.table.update(statics)
+
 
 
 
