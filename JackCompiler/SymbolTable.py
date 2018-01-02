1033a201251784e942ed2a000d6e599af32766b4
shimonheimowitz <heimy4prez@gmail.com>
2018-01-02 14:04:57 +0200

    working on symbol table

--- JackCompiler/SymbolTable.py
+++ JackCompiler/SymbolTable.py
@@ -7,12 +7,17 @@ in project 10. This module sotres and handles the following data:
 -> Whether the identifier is presently being defined (e.g. the identifier stands for a
     variable declared in a "var" statement) or used."""
 
-import numpy as np
+# import numpy as np
 
-NAME = 0
-TYPE = 1
-KIND = 2
-NUM  = 3
+# NAME = 0
+from enum import Enum
+TYPE = 0
+KIND = 1
+NUM  = 2
+
+class Table_Scopes(Enum):
+    class_scope     = "class"
+    method_scope    = "method"
 
 class SymbolTable:
     """
@@ -53,15 +58,23 @@ class SymbolTable:
         except KeyError:
             return -1
 
-
-
     def add_symbol(self, name, type, kind):
         assert name not in self.table.keys()
         if kind in self.counters.keys():
             num = self.counters[kind]
+            self.counters[kind] += 1
         else:
             raise ValueError("Unrecognized kind {}".format(kind))
         # np.append(self.table, [name, type, kind, num])
         self.table[name] = [type, kind, num]
+
+    def clean_non_static(self):
+        """
+        Erases all entries in this table except the static values
+        :return:
+        """
+        self.table = {k: v for k, v in self.table.items() if "static" == v[TYPE]}
+
+
 
 
